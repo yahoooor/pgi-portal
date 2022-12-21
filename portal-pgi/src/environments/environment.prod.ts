8969076dcad467a0d03658f3d43f95e69f064796
yahoooor <damaged7777@gmail.com>
2022-12-21 08:02:02 +0100

    fix

--- portal-pgi/src/environments/environment.prod.ts
+++ portal-pgi/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
 
   backendCors: "",
   backendUrl: "http://192.168.1.109:8000",
-  //backend:  "/api-backend" //"http://192.168.1.109:5000"
- backend: "http://127.0.0.1:5000"
+  backend:  "/api-backend" //"http://192.168.1.109:5000"
+ //backend: "http://127.0.0.1:5000"
 };
